62bb8b8abd71c89299228fc57bb0001e912827d4
Jan Juarez <janluis27@hotmail.com>
2020-10-20 18:38:41 -0500

    Tailwind y header

--- src/components/PxHeader.vue
+++ src/components/PxHeader.vue
@@ -1,16 +1,16 @@
 <template>
     <header class="shadow w-screen">
         <nav>
-            <nav class="flex items-center justify-between flex-wrap bg-red-800 p-6">
-                <div class="felx items-center flex-shrink-0 text-white mr-6">
-                    <div class="hidden sm:block w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-                        <div class="text-sm lg:flex-grow ">
-                            aaaaaaaaaaaaaaaaaa
-                            <img src="https://placekitten.com/50/50" class="relative left-0">
-                        </div>
+            <nav class="flex items-center justify-between flex-wrap bg-gradient-to-r from-red-600 p-6">
+                <img src="https://placekitten.com/50/50" class="relative left-0 ">
+                <div class="felx items-center flex-shrink-0 text-white text-lg mr-6 pl-5 ">
+                    Busqueda uwu
+                </div>
+                <div class="hidden sm:block w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+                    <div class="text-sm lg:flex-grow ">
+                        
                     </div>
                 </div>
-                
             </nav>
         </nav>
     </header>
