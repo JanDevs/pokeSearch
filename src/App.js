2acadd62a83f2a26058d90373f9125b55ad4e4eb
Jan Luis Juárez Avalos <janelproxd@gmail.com>
2024-01-18 00:51:54 -0600

    Modificada la base

--- src/App.js
+++ src/App.js
@@ -26,19 +26,7 @@ function MyNav(){
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          <MyNav />
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <MyNav />
     </div>
   );
 }
